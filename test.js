1b1443d4130ef155a21f684bf9d797186a1a2d02
wanganqing <905330374@qq.com>
2020-05-21 20:39:32 +0800

    fix: 暂存

---
+++ test.js
@@ -0,0 +1,34 @@
+// const arr = [1,2,3,4,5,6]
+
+// arr.forEach(item => {
+//     if(item ===3) continue
+//     console.log(item)
+// })
+
+// const a = arr.map(item => {
+//     if(item ===3) return
+    
+//     console.log(item)
+
+//     return item+1
+   
+// })
+// console.log(a)
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const _ = require('lodash');
+
+let outArr = [];
+try {
+    arr.map(obj => {
+        console.log(obj);
+        if (obj < 3) {
+            outArr.push(obj);
+        } else {
+            throw new Error('brack');
+        }
+    });
+    console.log(outArr)
+} catch (err) {
+    console.log(outArr)
+}
